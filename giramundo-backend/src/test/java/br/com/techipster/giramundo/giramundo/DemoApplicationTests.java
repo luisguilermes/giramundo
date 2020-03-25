533431602b2ad03945f95f42ecb9ac67d6081ea4
Luis Guilherme De Souza Silva <luisguilermes@gmail.com>
2020-03-25 17:17:58 -0300

    First commit

---
+++ giramundo-backend/src/test/java/br/com/techipster/giramundo/giramundo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.techipster.giramundo.giramundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
